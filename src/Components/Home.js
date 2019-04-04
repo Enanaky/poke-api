690eac5d7a381f13cf9c083932ba690f129471fe
Ignacio Petrone <ignacio@Santiagos-MacBook-Pro.local>
2019-04-04 13:59:48 +0200

    ultimo caambio

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -16,8 +16,6 @@ class Home extends React.Component {
 		return data;
 	}
 	render() {
-		const logo = require('../images/logo.png');
-
 		const gitLogo = require('../images/GitHub-Mark/PNG/GitHub.png');
 		const didYouKnow = this.props.didYouKnow;
 		if(didYouKnow){
@@ -34,7 +32,7 @@ class Home extends React.Component {
 						</div>
 						<div className="thanks">
 							<p>Gracias, de verdad, gracias a mi vieja, a mi viejo, a la produccion, al Chato, a Hope, a vos Marcelo, que hiciste esto posible! Gracias a mis fans y a los 3 fumancheros...</p>
-							<a href="https://github.com/Enanaky/poke-api" target="_blank"><img id="github-logo" src={gitLogo} alt="github"/></a>	
+							<a href="https://github.com/Enanaky/poke-api" rel="noopener noreferrer" target="_blank"><img id="github-logo" src={gitLogo} alt="github"/></a>	
 						</div>
 					</div>
 				);
