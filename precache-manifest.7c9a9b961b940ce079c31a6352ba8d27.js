2a7fb3b58c04674dbc5c46466afc88a61b91ecd6
Ignacio Petrone <juanignaciopetrone@gmail.com>
2019-04-14 22:36:18 +0200

    Updates

---
+++ precache-manifest.7c9a9b961b940ce079c31a6352ba8d27.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "724f4ab8c7a8e90c920388c14021f900",
+    "url": "/poke-api/static/media/trash.724f4ab8.png"
+  },
+  {
+    "revision": "023d617a1b733e2862bc8d439cface20",
+    "url": "/poke-api/static/media/logo.023d617a.png"
+  },
+  {
+    "revision": "831e1f4da157d96bc996f8c9f5f1e578",
+    "url": "/poke-api/static/media/Pokemon GB.831e1f4d.ttf"
+  },
+  {
+    "revision": "1ca52c6657d7df5c8ea8",
+    "url": "/poke-api/static/js/runtime~main.27597d65.js"
+  },
+  {
+    "revision": "ca505cd145a50d4c2338",
+    "url": "/poke-api/static/js/main.bd66c381.chunk.js"
+  },
+  {
+    "revision": "36352d252c1ba4c8de08",
+    "url": "/poke-api/static/js/2.d5341cd3.chunk.js"
+  },
+  {
+    "revision": "ca505cd145a50d4c2338",
+    "url": "/poke-api/static/css/main.63ce7c9e.chunk.css"
+  },
+  {
+    "revision": "b4627664b34bfcb769cd68509a7391b0",
+    "url": "/poke-api/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/poke-api/precache-manifest.7c9a9b961b940ce079c31a6352ba8d27.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/poke-api/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
