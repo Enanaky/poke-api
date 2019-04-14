2d9f87420059e07d2af9897d5f1c7995ecc37361
Ignacio Petrone <juanignaciopetrone@gmail.com>
2019-04-14 22:45:02 +0200

    lala

--- src/Components/App.js
+++ src/Components/App.js
@@ -19,61 +19,61 @@ class App extends Component {
       collection: null,
       didYouKnow: null,
       total: 0,
-      pokeFullDetails: null,      
+      pokeFullDetails: null,
       error: false,
     };
     //I allow functions to access "App component" 
     this.searchIt = this.searchIt.bind(this);
     this.pokeRandom = this.pokeRandom.bind(this);
   }
-  
+
   pokeRandom = () => {
     this.setState({
       didYouKnow: null,
       total: 0,
     })
-    for(let i=0; i<8; i++){
+    for (let i = 0; i < 8; i++) {
       const poke = Math.floor(Math.random() * 151) + 1;
       this.handleSearch(poke);
     }
   }
   // This is where the app checks if already have the pokemon.
-  checkIfIHaveIt = (pokeSearchCriteria) => {    
+  checkIfIHaveIt = (pokeSearchCriteria) => {
     const total = this.state.total;
-    if(total === 8){
+    if (total === 8) {
       this.handleSearch(pokeSearchCriteria);
-    }else{
+    } else {
       let gotIt = false;
       let poke = null;
       const copyCollection = Object.values(this.state.collection);
-      copyCollection.forEach(item => {                
-        if(item.id === parseInt(pokeSearchCriteria) || item.name === pokeSearchCriteria){
+      copyCollection.forEach(item => {
+        if (item.id === parseInt(pokeSearchCriteria) || item.name === pokeSearchCriteria) {
           gotIt = true;
           poke = item;
         }
-      });        
-      if(gotIt){
+      });
+      if (gotIt) {
         const copy = { ...this.state.collection };
-        copy[poke.id].visible = true;          
-        this.setState({ collection: copy})          
-      }else{
+        copy[poke.id].visible = true;
+        this.setState({ collection: copy })
+      } else {
         this.handleSearch(pokeSearchCriteria);
-      }        
-    }  
-  } 
+      }
+    }
+  }
   async handleSearch(pokeSearchCriteria) {
-    try{
+    try {
       const poke = await this.searchIt(pokeSearchCriteria);
       //  Persist poke in State
       this.savePokemon(poke);
-    }catch(err){}    
-  }  
+    } catch (err) { }
+  }
   // This method Fetch the data, make the poke-Model and persist data in state.
   async searchIt(searchData) {
     this.setState({
-      error: false, 
+      error: false,
     });
-    try {      
+    try {
       // 1. Fetch basic pokemon data
       const basicData = await pokeApi.getPokemon(searchData);
       // 2. Fetch Species Data with pokemon Name      
@@ -83,27 +83,27 @@ class App extends Component {
       // 4. Fetch Evolution Chain
       const evolutionData = await pokeApi.getResource(evolutionUrl);
       // 5. Compose Pokemon model
-      const pokemon = this.makePokemonCard(basicData, specieData, evolutionData);           
+      const pokemon = this.makePokemonCard(basicData, specieData, evolutionData);
       return pokemon;
       // 6. Catchin posibles errors
-    } catch(err) {
-      console.log(err); 
-      if(err) {
+    } catch (err) {
+      console.log(err);
+      if (err) {
         this.setState({
-          error: true, 
+          error: true,
         });
-      } 
-    }       
-  } 
+      }
+    }
+  }
   //POKEMON MODEL//p: basic / s: specie / e: evol
-  makePokemonCard = (p, s, e) => { 
+  makePokemonCard = (p, s, e) => {
     // console.log('p: ', p);
     // console.log('s: ', s);
     // console.log('e: ', e);    
     const pokemon = {
-      
+
       visible: true,
-      
+
       // Base
       id: p.id,
       name: p.name,
@@ -111,129 +111,129 @@ class App extends Component {
       weight: p.weight,
       types: p.types.map(item => item.type.name),
       images: { default: p.sprites.front_default, shiny: p.sprites.front_shiny },
-      stats: { 
+      stats: {
         speed: p.stats[0].base_stat,
-        special_defense: p.stats[1].base_stat,           
+        special_defense: p.stats[1].base_stat,
         special_attack: p.stats[2].base_stat,
         defense: p.stats[3].base_stat,
         attack: p.stats[4].base_stat,
         hp: p.stats[5].base_stat,
       },
-      
+
       // Species
-      color: s.color.name, 
+      color: s.color.name,
       egg_groups: s.egg_groups.map(item => item.name),
       genera: s.genera
-      .map(item => (item.language.name === 'en')? item.genus : 0)
-      .filter(item => item !== 0),
+        .map(item => (item.language.name === 'en') ? item.genus : 0)
+        .filter(item => item !== 0),
       generation: s.generation.name,
       habitat: s.habitat,
       description: s.flavor_text_entries
-      .map(item => (item.language.name === 'en') ? item.flavor_text : 0)
-      .filter(item => item !==0)[0],
-      
+        .map(item => (item.language.name === 'en') ? item.flavor_text : 0)
+        .filter(item => item !== 0)[0],
+
       // Evolution
-      evolution_chain: (this.evolutionChain(e.chain)),       
+      evolution_chain: (this.evolutionChain(e.chain)),
     }
     // console.log(pokemon);
-        
+
     return pokemon;
   }
   //Evolution chain Attribute Maker
-  evolutionChain(e) {  
+  evolutionChain(e) {
     const chain = [];
-    if(e.evolves_to.length === 0){        
-      chain.push(e.species.name);                
-    }else{
-      if(e.evolves_to.length === 1){
+    if (e.evolves_to.length === 0) {
+      chain.push(e.species.name);
+    } else {
+      if (e.evolves_to.length === 1) {
         const response = this.evolutionChain(e.evolves_to[0]);
         return [e.species.name, ...response];
-      }else{
+      } else {
         const array = e.evolves_to.map(item => item.species.name);
         return [e.species.name, array];
       }
     }
-    return chain;     
-  }  
+    return chain;
+  }
   // here is where the pokemon Object is storaged in State.
   savePokemon = (poke) => {
-    let plusOne = this.state.total; 
-    if(plusOne < 8){
+    let plusOne = this.state.total;
+    if (plusOne < 8) {
       const didYouKnowUpdated = { ...this.state.didYouKnow, [poke.id]: poke }
       plusOne++;
       this.setState({
         didYouKnow: didYouKnowUpdated,
         total: plusOne,
-      });      
-    }else{
+      });
+    } else {
       const collectionUpdated = { ...this.state.collection, [poke.id]: poke }
       plusOne++;
       this.setState({
         collection: collectionUpdated,
         total: plusOne,
       });
-    }  
-  }  
+    }
+  }
   hideDeck = () => {
     const copyCollection = Object.values(this.state.collection);
-    if(copyCollection){
-      copyCollection.forEach(item => {                
+    if (copyCollection) {
+      copyCollection.forEach(item => {
         this.hidePokemon(item.id);
-      }); 
+      });
     }
   }
   hidePokemon = (id) => {
     const collectionCopy = { ...this.state.collection };
     collectionCopy[id].visible = false;
-    this.setState({ collection: collectionCopy });    
+    this.setState({ collection: collectionCopy });
   }
   handleClickCard = (id) => {
     document.querySelector('.poke-details').classList.add('active');
     document.querySelector('.home').classList.remove('active');
     document.querySelector('.search').classList.remove('active');
-    const pokeRef = {...this.state.collection[id]}
+    const pokeRef = { ...this.state.collection[id] }
     this.setState({
       pokeFullDetails: pokeRef,
-    })        
+    })
   }
-  componentDidMount(){
+  componentDidMount() {
     this.pokeRandom();
     setInterval(this.pokeRandom, 30000);
   }
-  
+
   render() {
-    return(
+    return (
       <div className="container">
         <div className="app">
           <Header />
           <NavBar />
           <div className="body">
             <Switch>
-              <Route 
-                exact path="/" 
+              <Route
+                exact path={process.env.PUBLIC_URL + '/'}
                 render={props => <Home {...props}
                   didYouKnow={this.state.didYouKnow}
                   vevo={this.state.vevo}
                 />}
               />
-              <Route 
-                path="/Search" 
+              <Route
+                path="/Search"
                 render={props => <Search {...props}
-                  collection={this.state.collection} 
+                  collection={this.state.collection}
                   checkIfIHaveIt={this.checkIfIHaveIt}
                   hidePokemon={this.hidePokemon}
                   hideDeck={this.hideDeck}
                   handleClickCard={this.handleClickCard}
                 />}
-              />  
-              <Route 
-                exact path="/PokeDetails" 
+              />
+              <Route
+                exact path="/PokeDetails"
                 render={props => <PokeDetails {...props}
                   pokeFullDetails={this.state.pokeFullDetails}
                   getImgEvolutionChain={this.getImgEvolutionChain}
                 />}
               />
-            </Switch> 
+            </Switch>
           </div>
         </div>
       </div>
